384a8fcfa58ecd3df51abdad0d10c567a136193d
Felipe Garcia Alves <47622464+felipAlves@users.noreply.github.com>
2020-08-20 23:02:32 -0300

    Finalizando backend com adonisJs
    <ul>
      <li>Criado o AuthController para registrar usuario e realizar a autenticar </li>
    <li>
        Foi criado o ContactController onde se encontra o CRUD de contatos
    </li>
    <li>
        foi usado banco de dados mysql
    </li>
    </ul>
    
    Co-Authored-By: Felipe Garcia Alves <47622464+felipalves@users.noreply.github.com>

---
+++ backend/app/Controllers/Http/ContactController.js
@@ -0,0 +1,96 @@
+'use strict'
+
+
+const Contact = use('App/Models/Contact')
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with contacts
+ */
+class ContactController {
+  /**
+   * Show a list of all contacts.
+   * GET contacts
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index () {
+    const contacts = Contact.all()
+
+    return contacts
+  }
+
+
+  /**
+   * Create/save a new contact.
+   * POST contacts
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, auth }) {
+    const data = request.only(['name', 'email', 'city', 'uf', 'rua'])
+
+    const contact = await Contact.create({ user_id: auth.user.id, ...data})
+
+    return contact
+  }
+
+  /**
+   * Display a single contact.
+   * GET contacts/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show ({ params }) {
+    const contact = await Contact.findOrFail(params.id)
+
+    return contact
+  }
+
+
+  /**
+   * Update contact details.
+   * PUT or PATCH contacts/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ params, request }) {
+    const data = request.only(['name', 'email', 'city', 'uf', 'rua'])
+    const contact = await Contact.findOrFail(params.id)
+
+    contact.merge(data)
+
+    await contact.save()
+
+    return contact
+  }
+
+  /**
+   * Delete a contact with id.
+   * DELETE contacts/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params }) {
+    const contact = await Contact.findOrFail(params.id)
+
+    await contact.delete()
+  }
+}
+
+module.exports = ContactController
