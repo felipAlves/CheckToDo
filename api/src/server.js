f60e77a04366768983a928fee2612c2c891b68db
Felipe Garcia Alves <47622464+felipAlves@users.noreply.github.com>
2020-08-29 20:30:24 -0300

    previnindo aplicação contra ataques DDOS

--- api/src/server.js
+++ api/src/server.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const rateLimit = require("express-rate-limit");
 
 const routes = require('./routes')
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, 
+    max: 100 
+});
 
 mongoose.connect('mongodb+srv://felipealves:felipealves@cluster0.b2c4e.mongodb.net/<dbname>?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -13,6 +19,7 @@ const app = express()
 
 
 app.use(express.json())
+app.use(limiter);
 app.use(routes)
 
 
